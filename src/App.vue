c330c611c5680fe2b8f3567c1a6450f0d7918e46
YadinaM <yadina.moreira@gmail.com>
2023-12-06 17:58:10 +0100

    feat: add working navigation using routes

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,92 @@
 </script>
 
 <template>
-  <div>
-      app
-      <router-view></router-view>
-  </div>
+      <header>
+        <nav>
+            <div class="nav">
+                <div>
+                    <router-link to="/Shoe"><a><img src="./assets/logo.png" alt="terug naar home"></a></router-link>
+                </div>
+                <div class="nav__text">
+                    <router-link to="/Orders"><a>Orders</a></router-link>
+                    <router-link to="/"><a>Profile</a></router-link>
+                    <router-link to="/Orders"><a class="nav__text__order">Order Shoe</a></router-link>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div>
+        app
+        <a></a>
+        <router-view></router-view>
+    </div>
 </template>
 
 <style scoped>
+  :root{ /* css variabelen */
+      --colorText: black;
+      --colorButton: #F433FF;
+      --radius: 8px;
+      --fontFamily: sans-serif;
+  }
 
+  body{
+    font-family: var(--fontFamily);
+  }
+
+  nav{
+    margin-top: 1em;
+  }
+
+  .nav{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+  }
+
+  .nav img{
+      width: 90px;
+      margin-top: -1em;
+  }
+
+  .nav a{
+      text-decoration: none;
+      color: var(--colorText);
+      margin-left: 0.3em;
+      margin-right: 0.5em;
+  }
+
+  .nav__text{
+      margin-top: 1.2em;
+  }
+
+  .nav__text .nav__text__order{
+      color: white;
+      background-color: #F433FF;
+      padding: 0.5em 1em;
+      border-radius: 12px;
+  }
+
+  @media (min-width: 600px) {
+    .nav a{
+        text-decoration: none;
+        color: var(--colorText);
+    }
+
+    .nav img{
+      width: 140px;
+      margin-top: -2.5em;
+    }
+
+    .nav{
+        display: flex;
+        justify-content: space-between;
+        margin-left: 2em;
+        margin-right: 2em;
+    }
+
+    .nav__text .nav__text__order{
+        margin-right: 2em;
+    }
+  }
 </style>
