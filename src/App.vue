bc6b52d5e087ccaed35ce2228f7289fe5f55106d
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-12-08 10:47:04 +0100

    fix: delete nav on login

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
+  import { useRoute } from 'vue-router'
+  const route = useRoute()
 </script>
 
 <template>
-      <header>
+      <header v-if="route && route.path !== '/'">
         <nav>
             <div class="nav">
                 <div>
