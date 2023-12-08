7be1b29d80d35b58e5995508a4cc075a5ce56108
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-12-09 00:09:39 +0100

    fix: adjust typo

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
                 </div>
                 <div class="nav__text">
                     <router-link to="/Orders"><a>Orders</a></router-link>
-                    <router-link to="/AsjustPassword"><a>Profile</a></router-link>
+                    <router-link to="/AdjustPassword"><a>Profile</a></router-link>
                     <router-link to="/Shoe"><a class="nav__text__order">Order Shoe</a></router-link>
                 </div>
             </div>
