595a91a447feb9c33ee720a11014ae231cd86234
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-12-12 12:33:35 +0100

    fix: switch date sort logic

--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -29,14 +29,14 @@
     });
     const newestFilter = () => {
         orders.value.sort((a, b) => {
-            return new Date(a.date) - new Date(b.date);
+            return new Date(b.date) - new Date(a.date);
         });
     };
     newestFilter();
 
     const oldestFilter = () => {
         orders.value.sort((a, b) => {
-            return new Date(b.date) - new Date(a.date);
+            return new Date(a.date) - new Date(b.date);
         });
     };
     oldestFilter();
